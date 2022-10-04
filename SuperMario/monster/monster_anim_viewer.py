ba2dd5edd87d20cbdf40ab86d5d96f40db578802
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-10-04 09:56:49 +0900

    add green koopa

--- SuperMario/monster/monster_anim_viewer.py
+++ SuperMario/monster/monster_anim_viewer.py
@@ -12,13 +12,17 @@ def handle_events():
 bframe = 0
 gframe = 8
 rframe = 0
+grframe = 0
+
 rrframe = 0
 raction = 0
+graction = 0
 
 mbos = load_image('jr_coopa.png')
 goomba = load_image('Goomba.png')
 rkoopa = load_image('red_koopa.png')
 rrkoopa = load_image('red_koopa_wake.png')
+grkoopa = load_image('green_koopa.png')
 
 
 while running:
@@ -26,9 +30,11 @@ while running:
     # jump: character.clip_draw(frame * 50,  action , 50, 75, x, 110)
     mbos.clip_draw(bframe * 50,  60 , 50, 70, 500, 90)
     goomba.clip_draw(0,  gframe * 50 , 42, 50, 600, 90)
-    rkoopa.clip_draw(rframe * 25, 50 * raction, 25, 50, 900, 90)
-    rrkoopa.clip_draw(rrframe * 50, 0, 50, 48, 900, 200)
+    rkoopa.clip_draw(rframe * 25, 50 * raction, 25, 50, 950, 200)
+    rrkoopa.clip_draw(rrframe * 45, 0, 45, 44, 900, 200)
 
+    grkoopa.clip_draw(grframe * 25, 50 * graction, 25, 50, 990, 200)
+    
     update_canvas()
     handle_events()
 
@@ -37,11 +43,19 @@ while running:
     rframe =(rframe + 1) % 18
     rrframe =(rframe + 1) % 5
     
+    grframe =(grframe + 1) % 18
+    
     if rframe == 0:
         if raction == 0:
             raction = 1
         else:
             raction = 0
+   
+    if grframe == 0:
+        if graction == 0:
+            graction = 1
+        else:
+            graction = 0
    
     delay(0.1)
 close_canvas()
