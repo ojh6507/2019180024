b7d210338769be701fe7811ed993df488a63ad75
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-10-17 22:05:57 +0900

    add set_pos func in COIN

--- SuperMario/block/block.py
+++ SuperMario/block/block.py
@@ -1,3 +1,5 @@
+import random
+
 from pico2d import *
 
 class item_block:
@@ -12,13 +14,21 @@ class item_block:
 class COIN:
     def __init__(self):
         self.image =load_image('coin.png')
-        self.frame = 0
+        self.frame = random.randint(0,3)
+        self.count_anim = 0
+        self.x = 400
+        self.y = 90
     def draw(self):
-        self.image.clip_draw(self.frame * 25 ,0 ,25 ,25 ,400,90)
+        self.image.clip_draw(self.frame * 25 ,0 ,25 ,25 ,self.x, self.y)
+
     def update(self):
-        self.frame = (self.frame + 1) % 4
-
-
+        self.count_anim += 1
+        if self.count_anim == 3:
+            self.frame = (self.frame + 1) % 4
+            self.count_anim = 0
+    def set_pos(self,x,y):
+        self.x = x
+        self.y = y
 class Bricks:
     def __init__(self):
         self.image = load_image('block_2.png')
