1b84107c3834321d8275f02ae165a9dacc095cdb
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-12 03:35:39 +0900

    배경 변경 및 타일 추가

--- SuperMario/round1.py
+++ SuperMario/round1.py
@@ -1,55 +1,46 @@
 from pico2d import*
 import numpy as np
 import game_world
-INFO =np.zeros((34,254))
-for i in range(254):
-    INFO[2,i] = 1
 
-INFO[6,10] = 2
+INFO = np.zeros((13, 39))
+for i in range(39):
+    INFO[0,i] = 1
+
+INFO[0,8] = 0
+INFO[0,10] = 0
+INFO[0,25] = 0
+INFO[0,28] = 0
+INFO[0,30] = 0
+
+INFO[1,13] = 1
+INFO[0,13] = 2
+
+INFO[1,19] = 1
+INFO[0,19] = 2
+
+INFO[2,20] = 1
+INFO[1,20] = 2
+INFO[0,20] = 2
 
 print(INFO)
 
-TILESIZE = 30
-TILESIZE2 = 30
-def set_world():
-    for col in range(len(INFO)):
-        for row in range(len(INFO[col])):
-            if INFO[col][row] == 0:
-                empty = Empty_Tile(col, row)
-                game_world.add_object(empty,2)
-            elif INFO[col][row] == 1:
-                print(1)
-                ground = Floor_Tile(col, row, TILESIZE)
-                game_world.add_object(ground,3)
-            elif INFO[col][row] == 2:
-                print(1)
-                ground = Floor_Tile(col, row, TILESIZE2)
-                game_world.add_object(ground, 3)
 
-
-class Floor_Tile:
-    image = None
-    def get_name(self):
-        return 'background'
-
-    def __init__(self,col, row, size =1):
-        if Floor_Tile.image == None:
-            # Floor_Tile.image =load_image('test_tile.png')
-            Floor_Tile.image = load_image('upper_t1.png')
-        self.tilesize = size
-        self.x, self.y = row * self.tilesize, col * self.tilesize
-        self.rect_x, self.rect_y = self.x, self.y
-
-    def get_bb(self):
-        return self.x - 15, self.y - 15, self.x + 15, self.y + 15
-        # return self.y + self.size//2, self.y - self.size//2, self.x - self.size//2, self.x + self.size//2
-    def update(self):
-        pass
-    def draw(self):
-        self.image.draw(self.x, self.y)
-        draw_rectangle(*self.get_bb())
-
-        pass
+# def set_world():
+#
+#     for col in range(len(INFO)):
+#         for row in range(len(INFO[col])):
+#
+#             if INFO[col][row] == 0:
+#                 empty = Empty_Tile(col, row)
+#                 game_world.add_object(empty,2)
+#
+#             elif INFO[col][row] == 1:
+#                 ground = Floor_Tile(col, row)
+#                 game_world.add_object(ground,3)
+#
+#             elif INFO[col][row] == 2:
+#                 underground = under_Tile(col, row)
+#                 game_world.add_object(underground,3)
 
 class Empty_Tile:
     image = None
@@ -57,48 +48,48 @@ class Empty_Tile:
         return 'background'
 
     def __init__(self,col, row):
-        if Empty_Tile.image == None:
-            Empty_Tile.image =load_image('test_tile.png')
-
         self.x, self.y = row * 50, col * 50
         self.rect_x, self.rect_y = self.x, self.y
 
-
-    def get_rect(self):
-        #self.x - self.size = left
-        #self.x + self.size = right
-        pass
-        # return self.y + self.size//2, self.y - self.size//2, self.x - self.size//2, self.x + self.size//2
     def update(self):
         pass
     def draw(self):
         pass
 
-class Item_Tile:
-    def get_name(self):
-        return 'background'
 
-    def __init__(self,col, row, size):
-        self.tilesize = size
-        self.grid_x, self.grid_y = row * self.tilesize, col * self.tilesize
-        self.rect_x, self.rect_y = self.grid_x, self.grid_y
+class Floor_Tile(Empty_Tile):
+    image = None
+    def __init__(self,col, row):
+        if Floor_Tile.image == None:
+            Floor_Tile.image = load_image('ground.png')
+        self.x, self.y = row * 192, col * 77
 
-
-    def get_rect(self):
-        #self.x - self.size = left
-        #self.x + self.size = right
-        pass
-        # return self.y + self.size//2, self.y - self.size//2, self.x - self.size//2, self.x + self.size//2
+    def get_bb(self):
+        return self.x - 96, self.y - 40, self.x + 96, self.y + 40
     def update(self):
         pass
     def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+    def handle_collision(self,other,group):
         pass
+
+
+class under_Tile(Floor_Tile):
+    image = None
+
+    def __init__(self, col, row):
+        if under_Tile.image == None:
+            under_Tile.image = load_image('underground.png')
+        self.x, self.y = row * 192, col * 77
+
+
 
 class BACKGROUND:
     def get_name(self):
         return 'background'
     def __init__ (self):
-        self.image = load_image('background_2.png')
+        self.image = load_image('background.png')
         self.x = 3750
     def draw (self):
         self.image.draw(self.x, 500)
