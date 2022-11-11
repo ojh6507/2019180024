7ad9a05f7ab18d4a0f52d33f3d7cc489235d24a4
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-11 20:11:45 +0900

    블럭, 코인 애니메이션 frame time 적용

--- SuperMario/block/block.py
+++ SuperMario/block/block.py
@@ -1,6 +1,10 @@
 import random
 from pico2d import *
+import game_framework
 import game_world
+
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 class item_block:
     def get_name(self):
         return 'item_block'
@@ -10,20 +14,17 @@ class item_block:
         self.frame = 0
         self.x = 0
         self.y = 200
-        self.count_anim = 0
         self.moveup = False
     def draw(self):
-        self.image.clip_draw(self.frame * 30, 0, 30, 40, self.x, self.y)
+        self.image.clip_draw(int(self.frame) * 30, 0, 30, 40, self.x, self.y)
         draw_rectangle(*self.get_bb())
 
     def get_bb(self):
         return self.x - 10, self.y - 10, self.x + 10, self.y + 10
 
     def update(self):
-        self.count_anim += 1
-        if self.count_anim == 4:
-            self.frame = (self.frame+1)%4
-            self.count_anim = 0
+        self.frame = (self.frame + ACTION_PER_TIME * 4 * game_framework.frame_time) % 4
+
     def set_pos(self,x,y):
         self.x = x
         self.y = y
@@ -45,18 +46,15 @@ class COIN:
     def __init__(self):
         self.image =load_image('coin.png')
         self.frame = random.randint(0,3)
-        self.count_anim = 0
         self.x = 400
         self.y = 90
     def draw(self):
-        self.image.clip_draw(self.frame * 25 ,0 ,25 ,25 ,self.x, self.y)
+        self.image.clip_draw(int(self.frame) * 25 ,0 ,25 ,25 ,self.x, self.y)
         draw_rectangle(*self.get_bb())
 
     def update(self):
-        self.count_anim += 1
-        if self.count_anim == 3:
-            self.frame = (self.frame + 1) % 4
-            self.count_anim = 0
+        self.frame = (self.frame + ACTION_PER_TIME * 4 * game_framework.frame_time) % 4
+
     def set_pos(self,x,y):
         self.x = x
         self.y = y
@@ -76,19 +74,15 @@ class Bricks:
     def __init__(self):
         self.image = load_image('block_2.png')
         self.frame = 0
-        self.count_anim = 0
         self.x = 0
         self.y = 0
 
     def draw(self):
-        self.image.clip_draw(self.frame * 30, 0, 30, 40,self.x,self.y)
+        self.image.clip_draw(int(self.frame) * 30, 0, 30, 40,self.x,self.y)
         draw_rectangle(*self.get_bb())
 
     def update(self):
-        self.count_anim +=1
-        if self.count_anim == 4:
-            self.frame = (self.frame + 1) % 4
-            self.count_anim = 0
+        self.frame = (self.frame + ACTION_PER_TIME * 4 * game_framework.frame_time) % 4
 
     def set_pos(self, x, y):
         self.x = x
