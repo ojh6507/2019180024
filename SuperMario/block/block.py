9509d7ecb88f61b4619d7393bb484a038386b4f3
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-12 20:49:22 +0900

    block 충돌체크 디버깅

--- SuperMario/block/block.py
+++ SuperMario/block/block.py
@@ -5,6 +5,7 @@ import game_world
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+
 class item_block:
     def get_name(self):
         return 'item_block'
@@ -15,6 +16,7 @@ class item_block:
         self.x = 0
         self.y = 100
         self.moveup = False
+        self.mario_size = ' '
     def draw(self):
         self.image.clip_draw(int(self.frame) * 30, 0, 30, 40, self.x, self.y)
         draw_rectangle(*self.get_bb())
@@ -24,17 +26,25 @@ class item_block:
 
     def update(self):
         self.frame = (self.frame + ACTION_PER_TIME * 4 * game_framework.frame_time) % 4
-
     def set_pos(self,x,y):
         self.x = x
         self.y = y
     def returnY(self):
         return self.y
+    def get_size(self, size):
+        self.mario_size = size
 
     def handle_collision(self, other, group, p):
+        global PLAYER
         # print('ball disappear')
-        if group == 'player:item_blocks':
-            game_world.remove_object(self)
+        if group == 'player:item_block':
+            if p == 'top':
+                if self.mario_size == 'Small':
+                    print('gen mushroom!!')
+                else:
+                    print('gen flower!!')
+
+
 
 class COIN:
     def get_name(self):
