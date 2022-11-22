1da734e8c4729e44521fb27f5093090c6e690b30
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-22 11:22:50 +0900

    크래딧, 타이틀 state추가

---
+++ SuperMario/title_state.py
@@ -0,0 +1,35 @@
+import game_framework
+from pico2d import *
+import game_framework
+import play_state
+
+running = True
+image = None
+logo_time = 0.0
+
+def enter():
+    global image
+    image = load_image('tuk_credit.png')
+
+def exit():
+    global image
+    del image
+
+def update():
+    global logo_time
+    if logo_time > 1.0:
+        logo_time = 0
+        game_framework.quit()
+    delay(0.01)
+    logo_time += 0.01
+def draw():
+    clear_canvas()
+    image.draw(400,300)
+    update_canvas()
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            game_framework.change_state(play_state)
