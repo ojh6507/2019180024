3d256db814387a0d92cd0d2aba2f07dd5ae178f4
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-22 16:22:38 +0900

    clear함수에 collision clear추가

--- SuperMario/game_world.py
+++ SuperMario/game_world.py
@@ -34,6 +34,7 @@ def clear():
         del o
     for layer in objects:
         layer.clear()
+    collision_group.clear()
 
 def add_collision_group(a, b, group):
     if group not in collision_group:
