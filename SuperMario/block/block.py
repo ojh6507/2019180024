13a577b3871fc84d1ef9221fd83f070f650deb37
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-22 17:23:12 +0900

    아이템 생성, 코인 생성, 아이템 블럭 상태 변화

--- SuperMario/block/block.py
+++ SuperMario/block/block.py
@@ -3,10 +3,12 @@ from pico2d import *
 import game_framework
 import game_world
 import server
+from item import *
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 
 class item_block:
+    image = None
     def get_name(self):
         return 'item_block'
 
@@ -14,7 +16,8 @@ class item_block:
         self.x -= x
 
     def __init__(self):
-        self.image = load_image('block_1.png')
+        if item_block.image == None:
+            item_block.image = load_image('block_1.png')
         self.frame = 0
         self.x = 0
         self.y = 0
@@ -25,8 +28,12 @@ class item_block:
         self.Y_velocity = 0
         self.jump_height = 0
         self.type =' '
+        self.x_size = 30
+        self.y_size = 40
     def draw(self):
-        self.image.clip_draw(int(self.frame) * 30, 0, 30, 40, self.x, self.y)
+        # self.image.clip_draw(int(self.frame) * 30, 0, 30, 40, self.x, self.y)
+        self.image.clip_composite_draw(int(self.frame)*30, 0, 30, 40, 0, ' ', self.x, self.y,self.x_size,self.y_size)
+
         draw_rectangle(*self.get_bb())
     def up_box(self):
         self.y += self.Y_velocity * game_framework.frame_time
@@ -39,9 +46,21 @@ class item_block:
         return self.x - 16, self.y - 16, self.x + 16, self.y + 16
 
     def update(self):
-        self.frame = (self.frame + ACTION_PER_TIME * 4 * game_framework.frame_time) % 4
-        if self.up:
+        if self.up and self.available:
             self.up_box()
+        if self.citem:
+            self.gen_item()
+            self.citem = False
+        if not self.available:
+            self.image = load_image('unblock_1.png')
+            self.frame = 0
+            self.x_size = 30
+            self.y_size = 30
+        else:
+            self.frame = (self.frame + ACTION_PER_TIME * 4 * game_framework.frame_time) % 4
+            self.x_size = 30
+            self.y_size = 40
+
     def set_pos(self,x,y, type = 'coin'):
         self.x = x
         self.y = y
@@ -49,17 +68,32 @@ class item_block:
         self.jump_height = y + 10
         self.Y_velocity =self.jump_height
         self.type = type
+        self.available = True
+        self.citem = False
     def returnY(self):
         return self.y
     def gen_item(self):
-        if self.type =='item':
-            if server.player.mario_size == 'Small':
-                print('gen mushroom!!')
-            else:
-                print('gen flower!!')
+        if self.available:
+            if self.type =='item':
+                if server.player.mario_size == 'Small':
+                    Mushroom = MUSHROOM(self.x, self.y + 10)
+                    game_world.add_collision_group(server.player, Mushroom, 'player:mushroom')
+                    game_world.add_collision_group(Mushroom, server.ground, 'mushroom:ground')
+
+                    game_world.add_object(Mushroom, 1)
+                else:
+                    Flower = FLOWER(self.x, self.y + 10)
+                    # game_world.add_collision_group(server.player, Flower, 'player:flower')
+                    game_world.add_object(Flower, 1)
+            elif self.type == 'coin':
+                coin = COIN(self.x,self.y + 20, 'onblock')
+                game_world.add_object(coin,1)
+                print('gen coin!!!')
+            self.available = False
     def handle_collision(self, other, group, p):
         if group == 'player:item_block':
             if p == 'top':
+                self.citem =True
                 self.up = True
 
 
@@ -73,21 +107,29 @@ class COIN:
     def edit_x(self, x):
         self.x -= x
 
-    def __init__(self):
-        self.image =load_image('coin.png')
+    def up_coin(self):
+        self.y += self.Y_velocity * game_framework.frame_time
+        self.Y_velocity -= self.Y_gravity
+        if self.Y_velocity < 0:
+            game_world.remove_object(self)
+
+    def __init__(self ,x, y, gen = 'onground'):
+        self.image = load_image('coin.png')
         self.frame = random.randint(0,3)
-        self.x = 400
-        self.y = 90
+        self.x = x
+        self.y = y
+        self.jump_height = y + 5
+        self.Y_velocity = self.jump_height
+        self.Y_gravity = 5
+        self.gen = gen
     def draw(self):
         self.image.clip_draw(int(self.frame) * 25 ,0 ,25 ,25 ,self.x, self.y)
         draw_rectangle(*self.get_bb())
 
     def update(self):
+        if self.gen != 'onground':
+            self.up_coin()
         self.frame = (self.frame + ACTION_PER_TIME * 4 * game_framework.frame_time) % 4
-
-    def set_pos(self,x,y):
-        self.x = x
-        self.y = y
 
     def returnY(self):
         return self.y
