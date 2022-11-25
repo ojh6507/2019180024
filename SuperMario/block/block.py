182687774b0e63ef873fe155e828926dd0b6d8f5
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-26 00:29:03 +0900

    아이템, 블릭 블록 충돌 추가

--- SuperMario/block/block.py
+++ SuperMario/block/block.py
@@ -78,11 +78,14 @@ class item_block:
     def up_box(self):
         self.y += self.Y_velocity * game_framework.frame_time
         self.Y_velocity -= self.Y_gravity
+
         if self.y <= self.temp:
             self.up = False
             self.y = self.temp
             self.Y_velocity = self.jump_height
             self.available = False
+
+
     def get_bb(self):
         return self.x - 15, self.y - 15, self.x + 15, self.y + 15
 
@@ -90,9 +93,11 @@ class item_block:
         if self.x <= 800:
             if self.up and self.available:
                 self.up_box()
+
             if self.citem:
                 self.gen_item()
                 self.citem = False
+
             if not self.available:
                 self.image = load_image('unblock_1.png')
                 self.frame = 0
@@ -122,6 +127,7 @@ class item_block:
                     game_world.add_collision_group(server.player, Mushroom, 'player:mushroom')
                     game_world.add_collision_group(Mushroom, server.ground, 'mushroom:ground')
                     game_world.add_collision_group(Mushroom,self, 'mushroom:itemBox')
+                    game_world.add_collision_group(Mushroom, server.bricks, 'mushroom:brick')
 
                     game_world.add_object(Mushroom, 1)
                 else:
@@ -131,10 +137,9 @@ class item_block:
             elif self.type == 'coin':
                 coin = COIN(self.x,self.y + 20, 'onblock')
                 game_world.add_object(coin,1)
-                print('gen coin!!!')
     def handle_collision(self, other, group, p):
         if group == 'player:item_block':
-            if p == 'top':
+            if p == 'top' and not server.player.die:
                 self.citem =True
                 self.up = True
 
@@ -168,6 +173,7 @@ class COIN:
         self.image.clip_draw(int(self.frame) * 25 ,0 ,25 ,25 ,self.x, self.y)
         draw_rectangle(*self.get_bb())
 
+
     def update(self):
         if self.x <= 800:
             if self.gen != 'onground':
@@ -194,17 +200,50 @@ class Bricks:
         self.frame = 0
         self.x = 0
         self.y = 0
-
+        self.available = True
+        self.up = False
+        self.y_size = 40
+        self.jump_height = 0
+        self.Y_velocity = self.jump_height
+        self.Y_gravity = 15
+        self.temp = 0
     def draw(self):
-        self.image.clip_draw(int(self.frame) * 30, 0, 30, 40,self.x,self.y)
+        self.image.clip_composite_draw(int(self.frame) * 30, 0, 30, 40, 0, ' ', self.x, self.y, 30,
+                                       self.y_size)
         draw_rectangle(*self.get_bb())
 
     def update(self):
+        if self.up and self.available:
+            self.up_box()
+        if not self.available and self.op == 'destroy':
+            try:
+                game_world.remove_object(self)
+            except:
+                pass
+        elif not self.available and self.op == 'solid':
+            self.image = load_image('unblock_1.png')
+            self.frame = 0
+            self.y_size = 30
+
+
         self.frame = (self.frame + ACTION_PER_TIME * 4 * game_framework.frame_time) % 4
 
-    def set_pos(self, x, y):
+    def up_box(self):
+        self.y += self.Y_velocity * game_framework.frame_time
+        self.Y_velocity -= self.Y_gravity
+        if self.y <= self.temp:
+            self.up = False
+            self.y = self.temp
+            self.Y_velocity = self.jump_height
+
+            self.available = False
+    def set_pos(self, x, y, op = 'destroy'):
         self.x = x
         self.y = y
+        self.temp = y
+        self.op = op
+        self.jump_height = y + 10
+        self.Y_velocity = self.jump_height
 
     def returnY(self):
         return self.y
@@ -214,6 +253,7 @@ class Bricks:
     def handle_collision(self, other, group, pos):
         # print('ball disappear')
         if group == 'player:bricks':
-           pass
+           if pos =='top':
+             self.up = True
 
 
