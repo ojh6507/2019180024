73446732b52a20e33dfd7292baa983e5fff5e044
ojh6507 <45549589+ojh6507@users.noreply.github.com>
2022-11-30 02:26:23 +0900

    맵 추가

--- SuperMario/round2.py
+++ SuperMario/round2.py
@@ -1,6 +1,7 @@
 from pico2d import*
 import numpy as np
 import game_world
+import server
 
 INFO = np.zeros((13, 40))
 for i in range(40):
@@ -39,6 +40,7 @@ class Empty_Tile:
         return self.x - 98, self.y - 40, self.x + 98, self.y + 200
     def __init__(self,col, row):
         self.x, self.y = row * 192, col * 77
+        self.rect_x, self.rect_y = self.x, self.y
 
     def update(self):
         pass
@@ -53,11 +55,11 @@ class Floor_Tile(Empty_Tile):
     image = None
     def __init__(self,col, row):
         if Floor_Tile.image == None:
-            Floor_Tile.image = load_image('background/ground.png')
-        self.x, self.y = row * 192, col * 78
+            Floor_Tile.image = load_image('background/sand_ground.png')
+        self.x, self.y = row * 192, col * 77
 
     def get_bb(self):
-        return self.x - 97, self.y - 40, self.x + 97, self.y + 40
+        return self.x - 100, self.y - 40, self.x + 100, self.y + 40
     def update(self):
         pass
     def draw(self):
@@ -80,13 +82,19 @@ class BACKGROUND:
     def get_name(self):
         return 'background'
     def __init__ (self):
-        self.image = load_image('background/background_.png')
-        self.x = 3750
+        self.image = load_image('./background/background2.png')
+        self.canvas_width = get_canvas_width()
+        self.canvas_height = get_canvas_height()
+        self.w = self.image.w
+        self.h = self.image.h
+        self.x = self.w/2
+        self.y = self.h/2
+        self.window_bottom = 0
     def edit_x(self,x):
-        self.x-=x
+        self.x -= x
     def get_pos(self):
         return self.x
     def draw (self):
-        self.image.draw(self.x, 500)
+        self.image.draw(self.x,self.y)
     def update(self):
         pass
